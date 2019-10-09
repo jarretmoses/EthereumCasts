f733c1e228d13b74c2484627b27253c42666b318
Jarret Moses <jarret.moses@gmail.com>
2019-10-09 17:53:26 -0400

    test: add Inbox Contract tests

--- my-code/inbox/test/inbox.test.js
+++ my-code/inbox/test/inbox.test.js
@@ -1,5 +1,54 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+const { interface, bytecode } = require('../compile');
 
-const web3 = new Web3(ganache.provider);
+const web3 = new Web3(ganache.provider());
+
+
+describe('Inbox', () => {
+  let accounts;
+  let inbox;
+  let initialMessage = 'Hi there!';
+
+  beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+      .deploy({
+        data: bytecode,
+        arguments: [initialMessage]
+      })
+      .send({
+        from: accounts[0],
+        gas: '1000000'
+      });
+  });
+
+  describe('Deploy', () => {
+    it('deploys a contract', () => {
+      assert.ok(inbox.options.address);
+    });
+
+    it('has a default message', async () => {
+      const { methods: { message } } = inbox;
+      assert.equal(await message().call(), initialMessage);
+    });
+  });
+
+  describe('Update Message', () => {
+    it('should correctly update the message', async () => {
+      const {
+        methods: {
+          message,
+          setMessage
+        }
+      } = inbox;
+
+      const newMessage = 'New Message';
+
+      await setMessage(newMessage).send({ from: accounts[0] });
+
+      assert.equal(await message().call(), newMessage);
+    });
+  })
+})
